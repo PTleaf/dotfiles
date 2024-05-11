240b9590c6a14cc3cfcbb81cc5395a4eaca2d183
zhutingyi <zhutingyi@bytedance.com>
2024-05-11 16:59:49 +0800

    feat: 1

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ echo "Install Homebrew"
 if [ ! -x "$(command -v brew)" ]
 then
     echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 fi
 
 
 echo "Install with Brew Bundle"
 set +e
 export PATH=$PATH:/opt/homebrew/bin
-brew bundle
+brew bundle --file="./brewfile"
 set -e
